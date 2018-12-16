7c0122193fa4d9c09e830213d31869b96926ebb9
aijing <645982492@qq.com>
2018-12-16 22:24:57 +0800

    插入排序与二分查找

---
+++ ssm/Base/src/Sort/BinarySearch.java
@@ -0,0 +1,44 @@
+package Sort;
+
+import java.util.Scanner;
+//二分查找
+public class BinarySearch {
+	
+	public static void sort(int []arr) {
+		int len = arr.length;
+		for(int i = 0;i < len;i++) {
+			int left = 0;
+			int right = i-1;
+			int mid = 0;
+			int temp = arr[i];
+			while(right >= left) {
+				mid = (left + right)/2;
+				if(arr[mid] > temp) {  //要排序元素在已经排过序的左边
+					right = right - 1;
+				}else {
+					left = mid + 1;
+				}
+			}
+			for(int j = i -1;j > right;j--) {  //找到插入的位置
+				arr[j+1] = arr[j];
+			}
+			arr[right+1] = temp;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int []arr = new int[5];
+		for(int i = 0;i < 5;i++) {
+			arr[i] = sc.nextInt();
+		}
+		sort(arr);
+		for(int j = 0;j < 5;j++) {
+			System.out.println(arr[j]);
+		}
+
+	}
+
+}
---
+++ ssm/Base/src/Sort/InsertSort.java
@@ -0,0 +1,37 @@
+package Sort;
+
+import java.util.Scanner;
+
+//插入排序   时间复杂度O(n^2) 稳定排序
+public class InsertSort {
+	
+	public static void sort(int []arr) {
+		int len = arr.length;
+		for(int i = 0;i < len;i++) {
+			int key = arr[i];
+			int j = i-1;
+		while(j >= 0&&arr[j] > key) { //新拿到的数与已经排好序的数做比较
+			arr[j+1] = arr[j];
+			j--;
+		}
+		arr[j+1] = key;
+		
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int []arr = new int[5];
+		for(int i = 0;i < 5;i++) {
+			arr[i] = sc.nextInt();
+		}
+		sort(arr);
+		for(int j = 0;j < 5;j++) {
+			System.out.println(arr[j]);
+		}
+
+	}
+
+}
