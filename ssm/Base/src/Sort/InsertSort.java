0c20503d37a1ebd250b6904518dfe288c9d5a090
aijing <645982492@qq.com>
2019-02-19 20:20:24 +0800

    插入排序更改

--- ssm/Base/src/Sort/InsertSort.java
+++ ssm/Base/src/Sort/InsertSort.java
@@ -2,21 +2,23 @@ package Sort;
 
 import java.util.Scanner;
 
-//插入排序   时间复杂度O(n^2) 稳定排序
+//插入排序   时间复杂度O(n^2) 空间复杂度O(1) 稳定排序 原地排序
 public class InsertSort {
 	
 	public static void sort(int []arr) {
 		int len = arr.length;
-		for(int i = 0;i < len;i++) {
-			int key = arr[i];
-			int j = i-1;
-		while(j >= 0 && arr[j] > key) { //新拿到的数与已经排好序的数做比较
-			arr[j+1] = arr[j];
-			j--;
+		for(int i = 1;i < len;i++) {  //默认第一个元素已经排好序了,从第二个元素开始排序;
+			insertRight(arr,i);
 		}
-		arr[j+1] = key;
-		
+	}
+	
+	public static void insertRight(int[] arr,int i) {
+		int key = arr[i];    //将待排序的元素存入到临时变量中;
+		int j = i - 1;
+		for(;j >= 0 && arr[j] > key;j--) {
+			arr[j+1] = arr[j];  //大于待排序元素的往后移一位;
 		}
+		arr[j+1] = key;  //找到待排序元素的位置
 	}
 
 	public static void main(String[] args) {
